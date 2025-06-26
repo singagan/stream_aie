b54cccc482a0fcf07b418f5ba94d243cdf60051d
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2025-06-26 11:03:01 +0200

    formatting

--- stream/stages/codegen/aie_code_generation.py
+++ stream/stages/codegen/aie_code_generation.py
@@ -8,7 +8,6 @@ from xdsl.printer import Printer
 from zigzag.datatypes import Constants, LayerOperand
 
 from stream.compiler.dialects.stream import ComputationNodeOp, EdgeOp, EmptySSAValue, Stream, TransferOp
-from stream.compiler.transforms.aie_add_tracing_script import AIEAddTracingScript
 from stream.compiler.transforms.clear_memory_space import ClearMemorySpace
 from stream.compiler.transforms.convert_stream_to_aie import ConvertStreamToAIEPass
 from stream.cost_model.communication_manager import CommunicationLinkEvent
